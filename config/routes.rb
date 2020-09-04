3796be54679958c9c273c1d1dd14e5746c30a7a7
gustavo <gustavo26cs@gmail.com>
2020-09-04 16:31:55 -0300

    Add stylying to Welcome and Application.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  authenticate :user do
+  resources :docs
+  authenticated :user do
     root "docs#index", as: "authenticated_root"
   end
 
   root 'welcome#index'
-
-
-  resources :docs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
